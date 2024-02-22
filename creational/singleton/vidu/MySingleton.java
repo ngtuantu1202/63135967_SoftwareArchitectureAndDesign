12c9d47a3a80fa6c4d78759216abd9a0c82a2f5d
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2024-02-22 07:51:02 +0700

    vd1_singleton

---
+++ creational/singleton/vidu/MySingleton.java
@@ -0,0 +1,19 @@
+package creational.singleton.vidu;
+
+public class MySingleton {
+    //1. khai bao bien singleton
+    private static MySingleton instance;
+    int count =0;
+    //2. phuong thuc khoi tao private/protected
+    protected MySingleton() {
+    }
+    //3. phuong thuc tinh dung de tra ve bien instance
+    public static MySingleton getInstance(){
+        if(instance==null)
+            instance = new MySingleton();
+        return instance;
+    }
+    public void inCount(){
+        System.out.println(++count);
+    }
+}
