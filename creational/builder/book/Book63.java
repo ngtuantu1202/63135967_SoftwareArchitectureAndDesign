e7616331adc1f76057df89828ab615ba27b5e7bd
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2024-03-14 15:30:12 +0700

    Build_Book_unfinished

---
+++ creational/builder/book/Book63.java
@@ -0,0 +1,42 @@
+package creational.builder.book;
+
+import creational.builder.computer.Computer63;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book63 {
+    String ten, tacGia;
+    int soTrang;
+    List<Book63> dsChuong = new ArrayList<>();
+
+    private Book63(Builder b){
+        ten = b.ten;
+        tacGia = b.tacGia;
+        soTrang = b.soTrang;
+        dsChuong = b.dsChuong;
+    }
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Tựa đề: ").append(ten)
+                .append("Tác giả: ").append(tacGia)
+                .append("Số trang: ").append(soTrang)
+                .append("Danh sách Chương: ").append(dsChuong);
+        return "aa";
+    }
+    public static class Builder{
+        private  String ten, tacGia;
+        private int soTrang;
+        private List<Book63> dsChuong = new ArrayList<>();
+    }
+        public Book63.Builder tacGia(String ten){
+            return
+        }
+
+
+
+    public Book63 build(){
+        return new Book63(this);
+    }
+}
